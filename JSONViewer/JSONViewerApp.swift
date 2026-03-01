18cf4e41edbbbdc55145f96c031fef12c2ed4a8b
Akexorcist <akexorcist@gmail.com>
2026-03-01 18:20:58 +0700

    Initial Commit

---
+++ JSONViewer/JSONViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONViewerApp.swift
+//  JSONViewer
+//
+//  Created by Akexorcist on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct JSONViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
